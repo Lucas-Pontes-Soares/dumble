7290ab9e2150909d7c7d9a7c4f366486730e9004
Lucas Pontes Soares <eterneru.lucas@outlook.com>
2025-11-21 14:35:28 -0300

    Configurando api para deploy

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,15 +4,18 @@ import express, { Request, Response } from 'express';
 
 import cors from 'cors'; // cors = biblioteca da parte de segurança (controlar a origem basicamente)
 import routes from './routes';
-import { access } from 'fs';
 import * as path from 'path';
 
 
 const app = express(); // cria o servidor usando a biblioteca express
-const port = 3000; 
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173'], // se deixar o IP do meu pc, só ele acessa o server
+  origin: 
+  [
+    //'http://localhost:5173', 
+    'https://dumble-471f516b5939.herokuapp.com'
+  ], // se deixar o IP do meu pc, só ele acessa o server
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
