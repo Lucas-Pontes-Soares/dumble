7ff64cf68f80a0dbd23203c5768f26720955ea55
Lucas Pontes Soares <eterneru.lucas@outlook.com>
2025-11-24 10:04:17 -0300

    ajust in api

--- api/src/controllers/classes/getClassByTeacherId.ts
+++ api/src/controllers/classes/getClassByTeacherId.ts
@@ -38,7 +38,7 @@ export const getClassByTeacherId = async (req: AuthenticatedRequest, res: Respon
         );
 
         if (result.rowCount === 0) {
-            return res.status(404).json({ message: 'Classes not found.' });
+            return res.status(200).json({ success: false, message: 'Classes not found.' });
         }
         
         return res.status(200).json({ success: true, classes: result.rows });
